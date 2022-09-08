6ef1c1a2a71c7ec0184c1df4033d667a56e1896a
William Kraus <krwilliam03@gmail.com>
2022-09-07 22:22:55 -0300

    completar turmas.py em lista 1

--- lista_1/turmas.py
+++ lista_1/turmas.py
@@ -1,11 +1,24 @@
 
+from typing import Iterable
+
+
 class Pessoa:
-    def __init__(self, nome: str):
-        self.nome = nome
+    def __init__(self, id: int, nome: str):
+        self.__id = id
+        self.__nome = nome
+
+    @property
+    def id(self):
+        return self.__id
+
+    @property
+    def nome(self):
+        return self.__nome
+
 
 class Aluno(Pessoa):
-    def __init__(self, nome: str, email: str):
-        super().__init__(nome)
+    def __init__(self, id: int, nome: str, email: str):
+        super().__init__(id, nome)
         self.__email = email
         self.__iaa = 0
     
@@ -19,16 +32,30 @@ class Aluno(Pessoa):
 
 
 class Professor(Pessoa):
-    def __init__(self):
-        ...
+    def __init__(self, id: int, nome: str):
+        super().__init__(id, nome)
 
 
 class Turma:
     def __init__(self, disciplina: str):
-        self.disciplina = disciplina
+        self.__disciplina = disciplina
+        self.__profs = set()
+        self.__alunos = set()
+    
+    @property
+    def disciplina(self): return self.__disciplina
+
+    @property
+    def professores(self): return self.__profs
+
+    @property
+    def alunos(self): return self.__alunos
     
     def matricular_aluno(self, aluno: Aluno):
-        self.__alunos
+        self.__alunos.add(aluno)
+
+    def matricular_professor(self, professor: Professor):
+        self.__profs.add(professor)
 
 
 class Gerenciador:
@@ -41,19 +68,19 @@ class Gerenciador:
     
     def gerar_id(self):
         self.__id += 1
-        return self.__id
+        return self.__id        
     
     def cadastrar_aluno(self, nome: str, email: str):
-        aluno = Aluno(nome, email)
         matricula = self.gerar_id()
+        aluno = Aluno(matricula, nome, email)
 
         self.__alunos[matricula] = aluno
 
         return aluno
 
     def cadastrar_professor(self, nome: str):
-        professor = Professor(nome)
         matricula = self.gerar_id()
+        professor = Professor(matricula, nome)
 
         self.__professores[matricula] = professor
 
